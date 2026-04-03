b29ef673f1d607d60c17d565322d82f0aee4e646
Priyanshu Nimavat <priyanshu.nimavat45@gmail.com>
2026-04-03 19:28:57 +0530

    code updation for supabase connection in script.js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-window.onload = function() {
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+
+const supabase = createClient(
+  'https://idbratjfnpkzmbfzcehr.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkYnJhdGpmbnBrem1iZnpjZWhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzUyMTYyMzMsImV4cCI6MjA5MDc5MjIzM30._SHhi4Q7MTDE12L4tsl6yaLKAWvxZoVmmLZB5wdV59g'
+)
+window.addEventListener("load", function() {
     setTimeout(function() {
         document.getElementById("showcase-page").classList.add("hidden");
         const loginContainer = document.getElementById("login-container");
@@ -30,3 +36,33 @@ function verifyOTP() {
         alert("Invalid OTP. Please try again.");
     }
 }
+async function loginWithGoogle() {
+  try {
+    document.body.style.cursor = "wait";
+
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: 'https://care-buddy-pi.vercel.app/dashboard.html'
+      }
+    })
+
+    if (error) throw error;
+
+  } catch (err) {
+    alert("Error: " + err.message)
+  } finally {
+    document.body.style.cursor = "default";
+  }
+}
+async function checkUser() {
+  const { data: { user } } = await supabase.auth.getUser()
+
+  if (!user) {
+    window.location.href = "index.html"
+  } else {
+    console.log("User:", user)
+  }
+}
+
+checkUser()
